7f204eb595fca3c620ebae63c5d21bea85227f86
delceyhugo <53054509+delceyhugo@users.noreply.github.com>
2020-09-26 00:24:14 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*global app, $on */
+
+/**
+  * @module App
+*/
+(function () {
+	/**
+	 * Sets up a brand new Todo list.
+	 *
+	 * @function
+	 * @memberof module:App
+	 * @param {string} name The name of your new to do list.
+	 */
+	function Todo(name) {
+		this.storage = new app.Store(name);
+		this.model = new app.Model(this.storage);
+		this.template = new app.Template();
+		this.view = new app.View(this.template);
+		this.controller = new app.Controller(this.model, this.view);
+	}
+
+	var todo = new Todo('todos-vanillajs');
+
+	function setView() {
+		todo.controller.setView(document.location.hash);
+	}
+	$on(window, 'load', setView);
+	$on(window, 'hashchange', setView);
+})();
